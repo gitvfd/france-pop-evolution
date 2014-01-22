b77d6d751afe126a952ba2f40530c58479dcb9df
gitvfd <vinz2007@hotmail.com>
2014-01-22 15:04:07 +0100

    Update createScale.js

--- js/createScale.js
+++ js/createScale.js
@@ -1,6 +1,6 @@
 function createScale(){
 
-	d3.json("data/FrancePopDpt.json", function(error,data){
+        d3.json("data/FrancePopDpt.json", function(error,data){
 
 var minScale = d3.min(data.map(function(d) {return (d.value);} ))-1;
 var maxScale = d3.max(data.map(function(d) {return (d.value);} ))+1;
@@ -11,8 +11,8 @@ console.log(maxScale);
 var margin=115;
 
 var color = d3.scale.threshold()
-	    .domain([minScale,minScale+(maxScale-minScale)/9,minScale+2*(maxScale-minScale)/9,minScale+3*(maxScale-minScale)/9,minScale+4*(maxScale-minScale)/9,minScale+5*(maxScale-minScale)/9,minScale+6*(maxScale-minScale)/9,minScale+7*(maxScale-minScale)/9,minScale+8*(maxScale-minScale)/9,maxScale])
-	    .range(["#fff","#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84", "#fc8d59", "#ef6548","#d7301f","#b30000","#7f0000"]);
+            .domain([minScale,minScale+(maxScale-minScale)/9,minScale+2*(maxScale-minScale)/9,minScale+3*(maxScale-minScale)/9,minScale+4*(maxScale-minScale)/9,minScale+5*(maxScale-minScale)/9,minScale+6*(maxScale-minScale)/9,minScale+7*(maxScale-minScale)/9,minScale+8*(maxScale-minScale)/9,maxScale])
+            .range(["#fff","#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84", "#fc8d59", "#ef6548","#d7301f","#b30000","#7f0000"]);
 
 // A position encoding for the key only.
 var x = d3.scale.linear()
@@ -48,10 +48,17 @@ g2.selectAll("rect")
     .attr("width", function(d) { return d.x1 - d.x0; })
     .style("fill", function(d) { return d.z; });
 
+
 g2.call(xAxis).append("text")
     .attr("class", "caption")
     .attr("x", 0)
     .attr("y", 9)
-    .text("Population:");
+    .text("Legend:");
+
+g2.call(xAxis).append("text")
+    .attr("class", "legendtext")
+    .attr("x", width-2*margin+25)
+    .attr("y", 21)
+    .text("inhabitants");    
 });
 };
